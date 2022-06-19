8ee27c259c1a9dd6f9ed948a3e42792cfb9d997a
NwaObed <Obed.Nwachukwu@warwick.ac.uk>
2022-06-19 18:08:43 +0100

    author added

--- I4G000634QKH/blog/models.py
+++ I4G000634QKH/blog/models.py
@@ -1,10 +1,13 @@
 from cgitb import text
 from turtle import title
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
+    user = get_user_model()
+    author = models.ForeignKey(user, on_delete=models.CASCADE)
     created_date = models.DateTimeField()
-    Published_date = models.DateTimeField()
+    published_date = models.DateTimeField()
